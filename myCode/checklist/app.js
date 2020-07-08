160e773ce7dbd82e7e376209ef83e2fcb3c19108
hoffnen <christoffer-wickman@hotmail.com>
2020-07-08 11:59:04 +0200

    *

---
+++ myCode/checklist/app.js
@@ -0,0 +1,12 @@
+// SELECTORS
+const clInput = document.querySelector(".cl-input");
+const clButton = document.querySelector(".cl-button");
+
+// EVENTLISTENERS
+clButton.addEventListener("click", addItem);
+
+//Functions
+
+function addItem(event) {
+  event.preventDefault();
+}
